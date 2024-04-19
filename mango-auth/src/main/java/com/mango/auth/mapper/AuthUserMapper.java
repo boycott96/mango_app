653745e5c0e4aa80959fc96beb56644214e34d3d
sunruiguang <420007900@qq.com>
2024-04-19 16:52:18 +0800

    fix

--- mango-auth/src/main/java/com/mango/auth/mapper/AuthUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.mango.auth.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mango.api.entity.AuthUser;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AuthUserMapper extends BaseMapper<AuthUser> {
-}
