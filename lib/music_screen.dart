f0d8ad4bfa59da59036f2c87475cee8836359bed
sunruiguang <420007900@qq.com>
2024-04-20 16:08:22 +0800

    fix: 增加页面和tabbar

---
+++ lib/music_screen.dart
@@ -0,0 +1,23 @@
+// Copyright 2021 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+const Widget divider = SizedBox(height: 10);
+
+// If screen content width is greater or equal to this value, the light and dark
+// color schemes will be displayed in a column. Otherwise, they will
+// be displayed in a row.
+const double narrowScreenWidthThreshold = 400;
+
+class MusicScreen extends StatelessWidget {
+  const MusicScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("音乐"),
+    );
+  }
+}
