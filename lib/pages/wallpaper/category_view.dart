77033a781066831dd85f4a15d7eb999cc093627b
boycott96 <420007900@qq.com>
2024-05-11 00:00:12 +0800

    完成壁纸分页加载

--- lib/pages/wallpaper/category_view.dart
+++ lib/pages/wallpaper/category_view.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_test/api/wallpaper.dart';
 import 'package:flutter_application_test/pages/wallpaper/category_list.dart';
+import 'package:flutter_application_test/pages/wallpaper/wallpaper_list.dart';
 
 class CategoryView extends StatefulWidget {
   const CategoryView({super.key});
@@ -74,43 +75,52 @@ class _CategoryViewState extends State<CategoryView>
           ),
         ),
         const SizedBox(height: 10), // 添加行与行之间的间距
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
+        Row(children: [
+          Wrap(
+            spacing: 10.0, // 调整子组件之间的间距
+            runSpacing: 10.0, // 调整行之间的间距
             children: _categoryList.asMap().entries.map((entry) {
               final e = entry.value;
-              return Container(
-                width: (MediaQuery.of(context).size.width - 32 - 40) *
-                    0.25, // 设置容器宽度为屏幕宽度的20%
-                height: 70,
-                margin: const EdgeInsets.only(right: 10), // 添加每个容器之间的间距
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(e['categoryUrl']),
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(0.5), // 黑色层的颜色和透明度
-                      BlendMode.srcOver, // 混合模式，此处表示黑色层在顶部
-                    ),
-                  ),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Text(
-                      e['name'],
-                      style: const TextStyle(
-                        color: Colors.white,
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              WallpaperList(categoryId: e['id'])));
+                },
+                child: Container(
+                  width: (MediaQuery.of(context).size.width - 32 - 40) *
+                      0.25, // 设置容器宽度为屏幕宽度的20%
+                  height: 70,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(e['categoryUrl']),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.5), // 黑色层的颜色和透明度
+                        BlendMode.srcOver, // 混合模式，此处表示黑色层在顶部
                       ),
-                      overflow: TextOverflow.ellipsis, // 设置文本宽度缩写
+                    ),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Text(
+                        e['name'],
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                        overflow: TextOverflow.ellipsis, // 设置文本宽度缩写
+                      ),
                     ),
                   ),
                 ),
               );
             }).toList(),
-          ),
-        ),
+          )
+        ]),
       ],
     );
   }
--- lib/pages/wallpaper/wallpaper_list.dart
+++ lib/pages/wallpaper/wallpaper_list.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_test/api/wallpaper.dart';
+import 'package:flutter_application_test/pages/wallpaper/wallpaper_detail.dart';
 
 class WallpaperList extends StatefulWidget {
   final int categoryId;
@@ -13,19 +14,40 @@ class WallpaperList extends StatefulWidget {
 class _WallpaperListState extends State<WallpaperList>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+  final ScrollController _scrollController = ScrollController();
+
   int page = 1;
+
+  List<dynamic> _wallpaperList = [];
 
   @override
   void initState() {
     super.initState();
-    getWallpaperList();
+    _scrollController.addListener(_scrollListener);
+
+    getWallpaperList(page);
     _controller = AnimationController(vsync: this);
   }
-  
-  getWallpaperList() async{
-    Response response  = await WallpaperService(context).wallpaperForCategory(widget.categoryId, page);
+
+  void _scrollListener() {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      // Reach the bottom
+      setState(() {
+        page = page + 1;
+        getWallpaperList(page);
+      });
+    }
+  }
+
+  getWallpaperList(int pageNum) async {
+    Response response = await WallpaperService(context)
+        .wallpaperForCategory(widget.categoryId, pageNum);
     if (response.data['code'] == 0) {
-      print(response.data['data']);
+      var newDataList = response.data['data'];
+      setState(() {
+        _wallpaperList = [..._wallpaperList, ...newDataList];
+      });
     }
   }
 
@@ -41,7 +63,55 @@ class _WallpaperListState extends State<WallpaperList>
       appBar: AppBar(
         title: const Text("Wallpaper"),
       ),
-      body: Text("${widget.categoryId}")
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            children: [
+              Expanded(
+                child: Wrap(
+                  spacing: 10.0, // 调整子组件之间的间距
+                  runSpacing: 10.0, // 调整行之间的间距
+                  children: _wallpaperList
+                      .map(
+                        (e) => GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        WallpaperDetail(id: e['id'])));
+                          },
+                          child: Container(
+                            alignment: Alignment.bottomCenter,
+                            height: 200,
+                            width:
+                                (MediaQuery.of(context).size.width - 32 - 25) *
+                                    0.33,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                image: NetworkImage(e['thumbnailLarge']),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            child: Text(
+                              e['resolution'],
+                              style: const TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
